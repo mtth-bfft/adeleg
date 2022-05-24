11ccb6193e67c2770f7b18d94482f453cfa97fc2
Matthieu Buffet <matthieu@buffet.re>
2022-05-24 23:30:21 +0200

    Add application icon

--- adeleg/build.rs
+++ adeleg/build.rs
@@ -1,5 +1,33 @@
-extern crate embed_resource;
+extern crate winres;
 
 fn main() {
-    embed_resource::compile("adeleg.rc");
+  if cfg!(target_os = "windows") {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("icons\\app.ico");
+    res.set("FileDescription", env!("CARGO_PKG_DESCRIPTION"));
+    res.set_manifest(r#"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<assemblyIdentity
+    version="1.0.0.0"
+    processorArchitecture="*"
+    name="app"
+    type="win32"
+/>
+<dependency>
+    <dependentAssembly>
+        <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="*"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        />
+    </dependentAssembly>
+</dependency>
+</assembly>
+    "#);
+    res.compile().unwrap();
+  }
 }
