4a96624f398e91c89efd94c18a523cc226105f7e
Matthieu Buffet <matthieu@buffet.re>
2023-03-19 19:53:40 +0100

    Bump clap dependency to v4.1.11

--- adeleg/src/main.rs
+++ adeleg/src/main.rs
@@ -8,8 +8,7 @@ mod gui;
 use std::io::Write;
 use std::collections::HashMap;
 use engine::PrincipalType;
-use windows::Win32::Networking::Ldap::LDAP_PORT;
-use clap::{Command, Arg};
+use clap::{Parser, ValueEnum};
 use authz::Sid;
 use winldap::connection::LdapConnection;
 use crate::gui::run_gui;
@@ -17,117 +16,82 @@ use crate::engine::{Engine, AdelegResult};
 use crate::error::AdelegError;
 use crate::delegations::DelegationLocation;
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum IndexViewBy {
+    /// For each resource, show who can do something with it
+    Resource,
+    /// For each trustee, show where it can do something
+    Trustee,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct CliArgs {
+   /// (explicit server) LDAP server hostname or IP
+   #[arg(short, long)]
+   server: Option<String>,
+
+   /// (explicit server) LDAP port
+   #[arg(long, default_value_t=389)]
+   port: u16,
+
+   /// (explicit credentials) Logon user name
+   #[arg(long, requires="password")]
+   username: Option<String>,
+
+   /// (explicit credentials) Logon domain name
+   #[arg(long, requires="username")]
+   domain: Option<String>,
+
+   /// (explicit credentials) Logon password or * to prompt interactively
+   #[arg(long, requires="domain")]
+   password: Option<String>,
+
+   /// json file with delegation templates
+   #[arg(short, long, value_name="T.json")]
+   #[arg(number_of_values(1))]
+   templates: Vec<String>,
+
+   /// json file with delegations
+   #[arg(short, long, value_name="D.json")]
+   #[arg(number_of_values(1))]
+   delegations: Vec<String>,
+
+   /// Index view by trustee or resources (default is by resources)
+   #[arg(short, long, value_enum, default_value_t=IndexViewBy::Resource)]
+   index: IndexViewBy,
+
+    /// Output as text (default is GUI if there is no commandline argument
+    #[arg(long, default_value_t=false)]
+    text: bool,
+
+    /// Write output into a CSV file
+    #[arg(short, long)]
+    csv: Option<String>,
+
+    /// Include built-in delegations in the output
+    #[arg(long, default_value_t=false)]
+    show_builtin: bool,
+
+    /// Show unreadable security descriptors as warnings
+    #[arg(long, default_value_t=false)]
+    show_warning_unreadable: bool,
+
+    /// Show raw unresolved ACE contents
+    #[arg(long, default_value_t=false)]
+    show_raw: bool,
+}
+
 fn main() {
     if std::env::args().count() <= 1 {
         run_gui();
         return;
     }
-
-    let default_port = format!("{}", LDAP_PORT);
-    let app = Command::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(
-            Arg::new("server")
-                .help("(explicit server) LDAP server hostname or IP")
-                .long("server")
-                .short('s')
-                .number_of_values(1)
-        )
-        .arg(
-            Arg::new("port")
-                .help("(explicit server) LDAP port")
-                .long("port")
-                .number_of_values(1)
-                .default_value(&default_port)
-        )
-        .arg(
-            Arg::new("domain")
-                .help("(explicit credentials) Logon domain name")
-                .long("domain")
-                .short('d')
-                .number_of_values(1)
-                .requires_all(&["username", "password"])
-        )
-        .arg(
-            Arg::new("username")
-                .help("(explicit credentials) Logon user name")
-                .long("user")
-                .short('u')
-                .number_of_values(1)
-                .requires_all(&["domain","password"])
-        )
-        .arg(
-            Arg::new("password")
-                .help("(explicit credentials) Logon Password")
-                .long("password")
-                .short('p')
-                .number_of_values(1)
-                .requires_all(&["domain","username"])
-        )
-        .arg(
-            Arg::new("templates")
-                .help("json file with delegation templates")
-                .long("templates")
-                .short('t')
-                .value_name("T.json")
-                .multiple_occurrences(true)
-                .number_of_values(1)
-        )
-        .arg(
-            Arg::new("delegations")
-                .help("json file with delegations")
-                .long("delegations")
-                .short('D')
-                .value_name("D.json")
-                .multiple_occurrences(true)
-                .number_of_values(1)
-        ).arg(
-            Arg::new("index")
-                .help("Index view by trustee or resources (default is by resources)")
-                .long("index")
-                .takes_value(true)
-                .default_value("resources")
-                .possible_values(&["resources", "trustees"])
-        ).arg(
-            Arg::new("text")
-            .help("Output as text (default is GUI if there is no commandline argument")
-            .long("text")
-            .takes_value(false)
-        ).arg(
-            Arg::new("csv")
-                .help("Write output into a CSV file")
-                .takes_value(true)
-                .number_of_values(1)
-                .long("csv")
-        ).arg(
-            Arg::new("show_builtin")
-            .help("Include built-in delegations in the output")
-            .long("show-builtin")
-        ).arg(
-            Arg::new("show_warning_unreadable")
-            .help("Show unreadable security descriptors as warnings")
-            .long("show-warning-unreadable")
-        ).arg(
-            Arg::new("show_raw")
-                .help("Show unresolved ACE contents")
-                .long("show-raw")
-        );
-
-    let args = app.get_matches();
-    let server= args.value_of("server");
-    let port = args.value_of("port").expect("no port set");
-    let port = match port.parse::<u16>() {
-        Ok(n) if n > 0 => n,
-        _ => {
-            eprintln!("Unable to parse \"{}\" as TCP port", port);
-            std::process::exit(1);
-        }
-    };
+    let args = CliArgs::parse();
     let mut password = String::with_capacity(100);
-    let credentials = match (args.value_of("domain"),
-                             args.value_of("username"),
-                             args.value_of("password")) {
+    let credentials = match (args.domain.as_deref(),
+                             args.username.as_deref(),
+                             args.password.as_deref()) {
         (Some(d), Some(u), None) | (Some(d), Some(u), Some("*")) => {
             crate::utils::read_password(&mut password, &format!("Password for {}\\{}", d, u));
             Some((d, u, password.as_str()))
@@ -135,9 +99,8 @@ fn main() {
         (Some(d), Some(u), Some(p)) => Some((d, u, p)),
         _ => None,
     };
-
-    let (server, port) = if let Some(server) = server {
-        (server.to_owned(), port)
+    let (server, port) = if let Some(server) = args.server {
+        (server.clone(), args.port)
     } else {
         if let Some((server, port)) = utils::get_gc_domain_controller() {
             (server, port)
@@ -146,7 +109,6 @@ fn main() {
             std::process::exit(1);
         }
     };
-
     let conn = match LdapConnection::new(&server, port, credentials) {
         Ok(c) => c,
         Err(e) => {
@@ -155,38 +117,34 @@ fn main() {
         }
     };
 
-    let mut engine = Engine::new(&conn, !args.is_present("show_raw"));
+    let mut engine = Engine::new(&conn, !args.show_raw);
     engine.load_delegation_json(engine::BUILTIN_ACES).expect("unable to parse builtin delegations");
 
-    if let Some(input_filepaths) = args.values_of("templates") {
-        for input_filepath in input_filepaths.into_iter() {
-            let json = match std::fs::read_to_string(input_filepath) {
-                Ok(s) => s,
-                Err(e) => {
-                    eprintln!(" [!] Unable to open template file {} : {}", input_filepath, e);
-                    std::process::exit(1);
-                }
-            };
-            if let Err(e) = engine.load_template_json(&json) {
-                eprintln!(" [!] Unable to parse template file {} : {}", input_filepath, e);
+    for input_filepath in &args.templates {
+        let json = match std::fs::read_to_string(input_filepath) {
+            Ok(s) => s,
+            Err(e) => {
+                eprintln!(" [!] Unable to open template file {} : {}", input_filepath, e);
                 std::process::exit(1);
             }
+        };
+        if let Err(e) = engine.load_template_json(&json) {
+            eprintln!(" [!] Unable to parse template file {} : {}", input_filepath, e);
+            std::process::exit(1);
         }
     }
 
-    if let Some(input_files) = args.values_of("delegations") {
-        for input_filepath in input_files.into_iter() {
-            let json = match std::fs::read_to_string(input_filepath) {
-                Ok(s) => s,
-                Err(e) => {
-                    eprintln!(" [!] Unable to open delegation file {} : {}", input_filepath, e);
-                    std::process::exit(1);
-                }
-            };
-            if let Err(e) = engine.load_delegation_json(&json) {
-                eprintln!(" [!] Unable to parse delegation file {} : {}", input_filepath, e);
+    for input_filepath in &args.delegations {
+        let json = match std::fs::read_to_string(input_filepath) {
+            Ok(s) => s,
+            Err(e) => {
+                eprintln!(" [!] Unable to open delegation file {} : {}", input_filepath, e);
                 std::process::exit(1);
             }
+        };
+        if let Err(e) = engine.load_delegation_json(&json) {
+            eprintln!(" [!] Unable to parse delegation file {} : {}", input_filepath, e);
+            std::process::exit(1);
         }
     }
 
@@ -198,10 +156,8 @@ fn main() {
         }
     };
 
-    let show_builtin = args.is_present("show_builtin");
-    let show_warning_unreadable = args.is_present("show_warning_unreadable");
     let mut warning_unreadable_count = 0;
-    if let Some(csv_path) = args.value_of("csv") {
+    if let Some(csv_path) = &args.csv {
         let output: Box<dyn std::io::Write> = if csv_path == "-" {
             Box::new(std::io::stdout())
         } else {
@@ -226,7 +182,7 @@ fn main() {
                 Ok(r) => r,
                 Err(e) => {
                     warning_unreadable_count += 1;
-                    if show_warning_unreadable {
+                    if args.show_warning_unreadable {
                         writer.write_record(&[
                             location.to_string().as_str(),
                             "Global",
@@ -300,7 +256,7 @@ fn main() {
                 ]).expect("unable to write CSV record");
             }
             for (deleg, trustee, aces_found, aces_missing) in &res.delegations {
-                if deleg.builtin && !show_builtin {
+                if deleg.builtin && !args.show_builtin {
                     continue;
                 }
                 let (dn, ptype) = engine.resolve_sid(trustee).unwrap_or((trustee.to_string(), PrincipalType::External));
@@ -335,11 +291,11 @@ fn main() {
 
         drop(writer);
         let _ = std::io::stdout().flush();
-        if !show_warning_unreadable && warning_unreadable_count > 0 {
+        if !args.show_warning_unreadable && warning_unreadable_count > 0 {
             eprintln!("\n [!] {} security descriptors could not be read, use --show-warning-unreadable to see where", warning_unreadable_count);
         }
     }
-    else if args.value_of("index").unwrap_or("") == "trustees" {
+    else if args.index == IndexViewBy::Trustee {
         let mut warning_count = 0;
         let mut reindexed: HashMap<Sid, HashMap<DelegationLocation, AdelegResult>> = HashMap::new();
         for (location, res) in res.into_iter() {
@@ -385,7 +341,7 @@ fn main() {
                     entry.orphan_aces.push(ace);
                 }
                 for (deleg, trustee, aces_found, aces_missing) in res.delegations {
-                    if deleg.builtin && !show_builtin {
+                    if deleg.builtin && !args.show_builtin {
                         continue;
                     }
                     let entry = reindexed.entry(trustee.clone())
@@ -427,7 +383,7 @@ fn main() {
                     ));
                 }
                 for (delegation, _, aces_found, aces_missing) in &res.delegations {
-                    if !show_builtin && delegation.builtin {
+                    if !args.show_builtin && delegation.builtin {
                         continue;
                     }
                     println!("            Documented delegation: {}",
@@ -468,12 +424,12 @@ fn main() {
         res.sort_by(|(loc_a, _), (loc_b, _)| loc_a.cmp(loc_b));
         for (location, res) in res {
             if let Ok(res) = &res {
-                if !res.needs_to_be_displayed(show_builtin) {
+                if !res.needs_to_be_displayed(args.show_builtin) {
                     continue;
                 }
             } else {
                 warning_unreadable_count += 1;
-                if !show_warning_unreadable {
+                if !args.show_warning_unreadable {
                     continue;
                 }
             }
@@ -526,10 +482,10 @@ fn main() {
                 }
             }
             if res.delegations.iter().any(|(d, _, _, _)| !d.builtin) ||
-                    (!res.delegations.is_empty() && show_builtin) {
+                    (!res.delegations.is_empty() && args.show_builtin) {
                 println!("       Documented delegations:");
                 for (delegation, trustee, aces_found, aces_missing) in &res.delegations {
-                    if !show_builtin && delegation.builtin {
+                    if !args.show_builtin && delegation.builtin {
                         continue;
                     }
                     println!("         {} : {}", engine.resolve_sid(&trustee).map(|(dn, _)| dn).unwrap_or(trustee.to_string()),
@@ -560,7 +516,7 @@ fn main() {
             }
         }
 
-        if !show_warning_unreadable && warning_unreadable_count > 0 {
+        if !args.show_warning_unreadable && warning_unreadable_count > 0 {
             let _= std::io::stdout().flush();
             eprintln!("\n [!] {} security descriptors could not be read, use --show-warning-unreadable to see where", warning_unreadable_count);
         }
