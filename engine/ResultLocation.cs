d26bdf979a7a08152803132f9d6588b173989dcb
Matthieu Buffet <matthieu@buffet.re>
2025-10-23 10:06:19 +0200

    Fix ResultLocation serializer, and move to dedicated files
    Json serialization doesn't do dynamic type check, it just calls the
    serializer it knows for the generic class it's given. So we'll need
    to do a switch-case on the dynamic type inside the serializer.
    
    Because of this, the class has grown too much and needs a dedicated
    file. Also move ResultTrustee for good measure.

---
+++ engine/ResultLocation.cs
@@ -0,0 +1,113 @@
+﻿using adeleg.engine.connector;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace adeleg.engine
+{
+    public abstract class ResultLocation
+    {
+        ObjectClass _type;
+        public ObjectClass Type
+        {
+            get
+            {
+                return _type;
+            }
+        }
+        public abstract string HierarchicalDisplayName
+        {
+            get;
+        }
+
+        public ResultLocation(ObjectClass type)
+        {
+            this._type = type;
+        }
+    }
+
+    public class ResultLocationSchemaDefaultSd : ResultLocation
+    {
+        private string _className;
+        public override string HierarchicalDisplayName { get => $"Default on new {this._className} objects"; }
+
+        public string ClassName
+        {
+            get
+            {
+                return _className;
+            }
+        }
+
+        public ResultLocationSchemaDefaultSd(string className) :
+            base(ObjectClass.Container)
+        {
+            this._className = className;
+        }
+    }
+
+    public class ResultLocationDn : ResultLocation
+    {
+        private string _dn;
+        public override string HierarchicalDisplayName { get => _dn; }
+        public string Dn { get => _dn; }
+
+        public ResultLocationDn(string dn, string mostSpecificClass)
+            : base(ObjectClassHelper.FromString(mostSpecificClass, false))
+        {
+            this._dn = dn;
+        }
+    }
+
+    public class ResultLocationDomainRelativeDn : ResultLocation
+    {
+        private string _domainRelativeDn;
+        public override string HierarchicalDisplayName { get => _domainRelativeDn; }
+
+        public ResultLocationDomainRelativeDn(string mostSpecificClass, string domainRelativeDn)
+            : base(ObjectClassHelper.FromString(mostSpecificClass, false))
+        {
+            this._domainRelativeDn = domainRelativeDn;
+        }
+    }
+
+    public class ResultLocationRootDomainRelativeDn : ResultLocation
+    {
+        private string _rootDomainRelativeDn;
+        public override string HierarchicalDisplayName { get => _rootDomainRelativeDn; }
+
+        public ResultLocationRootDomainRelativeDn(string mostSpecificClass, string rootDomainRelativeDn)
+            : base(ObjectClassHelper.FromString(mostSpecificClass, false))
+        {
+            this._rootDomainRelativeDn = rootDomainRelativeDn;
+        }
+    }
+
+    public class ResultLocationSerializer : JsonConverter<ResultLocation>
+    {
+        public override ResultLocation Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, ResultLocation location, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            if (location is ResultLocationDn dn)
+            {
+                writer.WriteString("dn", dn.Dn);
+            }
+            else if (location is ResultLocationSchemaDefaultSd schema)
+            {
+                writer.WriteString("className", schema.ClassName);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+
+            writer.WriteEndObject();
+        }
+    }
+}
---
+++ engine/ResultTrustee.cs
@@ -0,0 +1,77 @@
+﻿using adeleg.engine.connector;
+using System;
+using System.Security.Principal;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace adeleg.engine
+{
+    public class ResultTrustee
+    {
+        private ObjectClass _type;
+        private string _dn;
+        private bool _isTier0;
+        private SecurityIdentifier _sid;
+        private int _domainRid;
+        private int _rootDomainRid;
+        private string _samAccountName;
+
+        public ObjectClass Type { get => _type; }
+        public string Dn { get => _dn; }
+        public string SamAccountName { get => _samAccountName; }
+        public SecurityIdentifier Sid { get => _sid; }
+        public bool IsTier0 { get => _isTier0; }
+        public int DomainRid { get => _domainRid; }
+        public int RootDomainRid { get => _rootDomainRid; }
+
+        /**
+         * When building a trustee from a live/dumped LDAP security descriptor, you will always have
+         * its SID (and, often, its type+DN+samaccountname and whether it should be considered Tier0).
+         * SID is used for access right computations, and the rest is just for pretty pretting.
+         * When building a trustee for generalized results, we often won't include the SID if it's
+         * a S-1-5-21-something-domain-specific which does not generalize at all.
+         */
+        public ResultTrustee(SecurityIdentifier sid = null, ObjectClass type = ObjectClass.UnknownTrustee, string dn = null, string samAccountName = null, bool isTier0 = false)
+        {
+            if (sid == null && dn == null && samAccountName == null)
+            {
+                throw new ArgumentNullException("SecurityIdentifier (can only be null if DN or SamAccountName is provided)");
+            }
+            _sid = sid;
+            _type = type;
+            _dn = dn;
+            _samAccountName = samAccountName;
+            _isTier0 = isTier0;
+            _domainRid = 0;
+            _rootDomainRid = 0;
+        }
+    }
+
+    public class ResultTrusteeSerializer : JsonConverter<ResultTrustee>
+    {
+        public override ResultTrustee Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, ResultTrustee trustee, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+
+            if (trustee.Dn != null)
+                writer.WriteString("dn", trustee.Dn);
+            if (trustee.Sid != null)
+                writer.WriteString("sid", trustee.Sid.ToString());
+            if (trustee.SamAccountName != null)
+                writer.WriteString("samaccountname", trustee.SamAccountName);
+            if (trustee.DomainRid != 0)
+                writer.WriteNumber("domain_rid", trustee.DomainRid);
+            if (trustee.RootDomainRid != 0)
+                writer.WriteNumber("root_domain_rid", trustee.RootDomainRid);
+            if (trustee.IsTier0)
+                writer.WriteBoolean("is_tier0", true);
+
+            writer.WriteEndObject();
+        }
+    }
+}
